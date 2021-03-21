04e28c3bb335ebfc0a59ff7e3b77c2b5ab20bed5
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2021-03-21 16:09:56 +0000

    Cleaned up API and internal data representation
    Now the properties within the GCI class are set as within the field
    self.__data[key], where the key is set based on the kwargs.
    This change resulted in a reduction in necessary getter and setter
    methods needed to be provided by the class and an overall cleaner
    interface.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![PyPI license](https://img.shields.io/pypi/l/pygcs.svg)](https://pypi.python.org/pypi/pygcs/)
-[![Generic badge](https://img.shields.io/badge/Version-v0.2.4-red.svg)](https://shields.io/)
+[![Generic badge](https://img.shields.io/badge/Version-v0.3.0-red.svg)](https://shields.io/)
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
 
 # Introduction
@@ -16,58 +16,82 @@ pip3 install pycgs
 
 # Usage
 
-The following shows how to calculate the GCI and get additional information that may be useful to establish grid independence.
+The following shows how to calculate the GCI and get additional information that may be useful to establish grid independence. Each GCI calculation requires the number of cells, the solution at each grid level and some information about the domain and its size. You can either specify directly a representative grid spacing (e.g. the square root of the cell area or cubed root of the cell volume in 2D or 3D, respectively) or the area / volume of the 2D / 3D domain. This, together with the number of cells, will then be used to calculate a representative grid size.
+
+Additional information are the dimension of the simulation (1D, 2D, 3D) and the order used in the simulation. All these quantities need to be passed to the constructor of the GCI class but can be altered later if the GCI class is used dynamically while simulations are running through setter functions.
+
+The following shows an example calculation taken from reference [1] and 3 different ways are presented how to calculated the GCI using either a fixed volume domain, a domain with potentially varying domain size and a domain for which we prescribe the representative size directly (and thus do not require any area / volume information about the simulation domain)
 
 ```python
 import pyGCS
 
-# number of cells per grid
-grids = [18000, 8000, 4500]
+# create grid convergence study (gcs) object either based on volume og grid size
+    # single volume
+    gcs_single_volume = pyGCS.GCI(dimension=2, simulation_order=2, volume=76, cells=[18000, 8000, 4500],
+                                  solution=[6.063, 5.972, 5.863])
 
-# volume of the simulation domain for each simulation
-volume = [76, 76, 76]
+    # potentially varying domain size
+    gcs_multiple_volumes = pyGCS.GCI(dimension=2, simulation_order=2, volume=[76, 76, 76], cells=[18000, 8000, 4500],
+                                     solution=[6.063, 5.972, 5.863])
 
-# integral quantity for which to calculate the GCI
-solution = [6.063, 5.972, 5.863]
+    # specify representative grid spacing directly
+    gcs_with_grid_size = pyGCS.GCI(dimension=2, simulation_order=2, grid_size=[0.75, 1.125, 1.5],
+                                   cells=[18000, 8000, 4500], solution=[6.063, 5.972, 5.863])
 
-# dimension of the simultion (here 2D)
-dimension = 2
+    # get GCI values (coarse-medium and medium-fine grid configuration)
+    gci_1 = gcs_single_volume.get('gci')
+    gci_2 = gcs_multiple_volumes.get('gci')
+    gci_3 = gcs_with_grid_size.get('gci')
 
-# create grid convergence study (gcs) object
-gcs = pyGCS.GCI(dimension, volume, grids, solution)
+    # get the asymptotic GCI (for three grid levels, it is a single value)
+    asymptotic_gci_1 = gcs_single_volume.get('asymptotic_gci')
+    asymptotic_gci_2 = gcs_multiple_volumes.get('asymptotic_gci')
+    asymptotic_gci_3 = gcs_with_grid_size.get('asymptotic_gci')
 
-# get GCI values (coarse-medium and medium-fine grid configuration)
-gci = gcs.get_gci()
+    # apparent (observed) order of the simulation (for three grid levels, it is a single value)
+    order_1 = gcs_single_volume.get('apparent_order')
+    order_2 = gcs_multiple_volumes.get('apparent_order')
+    order_3 = gcs_with_grid_size.get('apparent_order')
 
-# get the asymptotic GCI (for three grid levels, it is a single value)
-asymptotic_gci = gcs.get_asymptotic_gci()
+    # extrapolated value we would achieve for a fine enough grid with no grid induced errors
+    extrapolated_value_1 = gcs_single_volume.get('extrapolated_value')
+    extrapolated_value_2 = gcs_multiple_volumes.get('extrapolated_value')
+    extrapolated_value_3 = gcs_with_grid_size.get('extrapolated_value')
 
-# apparent (observed) order of the simulation (for three grid levels, it is a single value)
-order = gcs.get_order()
+    # calculated grid size for a specified GCI value
+    recommend_grid_size_1 = gcs_single_volume.get_number_of_cells_for_specified_gci_of(0.01)
+    recommend_grid_size_2 = gcs_multiple_volumes.get_number_of_cells_for_specified_gci_of(0.01)
+    recommend_grid_size_3 = gcs_with_grid_size.get_number_of_cells_for_specified_gci_of(0.01)
 
-# extrapolated value we would achieve for a fine enough grid with no grid induced errors
-extrapolated_value = gcs.get_extrapolated_value()
+    # GCI_32 = 4.11%
+    print(f'GCI (1st approach) for coarse to medium grid (GCI_32): {gci_1[1] * 100:.2f}%')
+    print(f'GCI (2nd approach) for coarse to medium grid (GCI_32): {gci_2[1] * 100:.2f}%')
+    print(f'GCI (3rd approach) for coarse to medium grid (GCI_32): {gci_3[1] * 100:.2f}%\n')
 
-# calculated grid size for a specified GCI value
-recommend_grid_size = gcs.get_number_of_cells_for_specified_gci_of(0.01)
-    
-# GCI_32 = 4.11%
-print(f'GCI for coarse to medium grid (GCI_32): {gci[1] * 100:.2f}%')
+    # GCI_21 = 2.17%
+    print(f'GCI (1st approach) for medium to fine   grid (GCI_21): {gci_1[0] * 100:.2f}%')
+    print(f'GCI (2nd approach) for medium to fine   grid (GCI_21): {gci_2[0] * 100:.2f}%')
+    print(f'GCI (3rd approach) for medium to fine   grid (GCI_21): {gci_3[0] * 100:.2f}%\n')
 
-# GCI_21 = 2.17%
-print(f'GCI for medium to fine   grid (GCI_21): {gci[0] * 100:.2f}%')
+    # asymptotic GCI = 1.015
+    print(f'asymptotic GCI value (1st approach, a value close to 1 indicates grid independence): {asymptotic_gci_1[0]:.3f}')
+    print(f'asymptotic GCI value (2nd approach, a value close to 1 indicates grid independence): {asymptotic_gci_2[0]:.3f}')
+    print(f'asymptotic GCI value (3rd approach, a value close to 1 indicates grid independence): {asymptotic_gci_3[0]:.3f}\n')
 
-# asymptotic GCI = 1.015
-print(f'asymptotic GCI value (a value close to 1 indicates grid independence): {asymptotic_gci[0]:.3f}')
+    # extrapolated value = 6.1685
+    print(f'Extrapolated value (1st approach): {extrapolated_value_1:.4f}')
+    print(f'Extrapolated value (2nd approach): {extrapolated_value_2:.4f}')
+    print(f'Extrapolated value (3rd approach): {extrapolated_value_3:.4f}\n')
 
-# extrapolated value = 6.1685
-print(f'Extrapolated value: {extrapolated_value:.4f}')
+    # order = 1.53
+    print(f'order achieved in simulation (1st approach): {order_1[0]:.2f}')
+    print(f'order achieved in simulation (2nd approach): {order_2[0]:.2f}')
+    print(f'order achieved in simulation (3rd approach): {order_3[0]:.2f}\n')
 
-# order = 1.53
-print(f'order achieved in simulation: {order[0]:.2f}')
-
-# recommend_grid_size = 29872
-print(f'Number of cells required to achieve a GCI of 1%: {recommend_grid_size:.0f}')
+    # recommend_grid_size = 29872
+    print(f'Number of cells required to achieve a GCI of 1% (1st approach): {recommend_grid_size_1:.0f}')
+    print(f'Number of cells required to achieve a GCI of 1% (2nd approach): {recommend_grid_size_2:.0f}')
+    print(f'Number of cells required to achieve a GCI of 1% (3rd approach): {recommend_grid_size_3:.0f}')
 ```
 
 # References
--- tests/test_GCI.py
+++ tests/test_GCI.py
@@ -2,121 +2,68 @@ import pytest
 import src.pyGCS as pyGCS
 
 
-def test_empty_constructor():
-    # arrange
-    sut = pyGCS.GCI()
-
-    # assert
-    assert sut.get_dimension() == 3
-    assert sut.get_safety_factor() == 1.25
-    assert len(sut.get_volume()) == 0
-    assert len(sut.get_cells()) == 0
-    assert len(sut.get_solution()) == 0
-
-
 def test_value_initialising_constructor():
     # arrange
-    sut = pyGCS.GCI(2, [10.45, 11.45, 12.45], [100, 200, 400], [0.8, 0.95, 1.0])
+    sut = pyGCS.GCI(dimension=2, volume=[10.45, 11.45, 12.45], cells=[100, 200, 400], solution=[0.8, 0.95, 1.0])
 
     # act
-    volume = sut.get_volume()
-    cells = sut.get_cells()
-    solution = sut.get_solution()
-    sut.set_safety_factor(3.0)
+    volume = sut.get('volume')
+    cells = sut.get('cells')
+    solution = sut.get('solution')
+    sut.set('safety_factor', 3.0)
 
     # assert
-    assert sut.get_dimension() == 2
-
-    assert sut.get_safety_factor() == 3.0
+    assert sut.get('dimension') == 2
+    assert sut.get('safety_factor') == 3.0
 
     assert len(volume) == 3
-    assert volume[0] == 10.45
+    assert volume[0] == 12.45
     assert volume[1] == 11.45
-    assert volume[2] == 12.45
+    assert volume[2] == 10.45
 
     assert len(cells) == 3
-    assert cells[0] == 100
+    assert cells[0] == 400
     assert cells[1] == 200
-    assert cells[2] == 400
+    assert cells[2] == 100
 
     assert len(solution) == 3
-    assert solution[0] == 0.8
+    assert solution[0] == 1.0
     assert solution[1] == 0.95
-    assert solution[2] == 1.0
-
-
-def test_volume_setter():
-    # arrange
-    sut = pyGCS.GCI()
-
-    # act
-    sut.set_volume([10.45, 15.55])
-
-    # assert
-    assert len(sut.get_volume()) == 2
-    assert sut.get_volume()[0] == 10.45
-    assert sut.get_volume()[1] == 15.55
-
-
-def test_cell_count_setter():
-    # arrange
-    sut = pyGCS.GCI()
-
-    # act
-    sut.set_cells([1, 2])
-
-    # assert
-    assert len(sut.get_cells()) == 2
-    assert sut.get_cells()[0] == 1
-    assert sut.get_cells()[1] == 2
-
-
-def test_solution_setter():
-    # arrange
-    sut = pyGCS.GCI()
-
-    # act
-    sut.set_solution([0.99, 1.0])
-
-    # assert
-    assert len(sut.get_solution()) == 2
-    assert sut.get_solution()[0] == 0.99
-    assert sut.get_solution()[1] == 1.0
+    assert solution[2] == 0.8
 
 
 @pytest.fixture
-def grid_parameter():
-    return {
-        'volume': [76, 76, 76],
-        'grids': [18000, 8000, 4500],
-        'solution': [6.063, 5.972, 5.863]
-    }
+def example_grid_celik():
+    return pyGCS.GCI(dimension=2, volume=[76, 76, 76], cells=[18000, 8000, 4500], solution=[6.063, 5.972, 5.863])
 
 
 @pytest.fixture
-def grid_parameter_reverse_order():
-    return {
-        'volume': [76, 76, 76],
-        'grids': [4500, 8000, 18000],
-        'solution': [5.863, 5.972, 6.063]
-    }
+def example_grid_celik_single_volume():
+    return pyGCS.GCI(dimension=2, volume=76, cells=[18000, 8000, 4500], solution=[6.063, 5.972, 5.863])
 
 
 @pytest.fixture
-def grid_parameter_random_order():
-    return {
-        'volume': [76, 76, 76],
-        'grids': [8000, 18000, 4500],
-        'solution': [5.972, 6.063, 5.863]
-    }
+def example_grid_celik_reverse_order():
+    return pyGCS.GCI(dimension=2, volume=[76, 76, 76], cells=[4500, 8000, 18000], solution=[5.863, 5.972, 6.063])
 
 
-def test_gci_calculation_based_on_volume(grid_parameter):
+@pytest.fixture
+def example_grid_celik_random_order():
+    return pyGCS.GCI(dimension=2, volume=[76, 76, 76], cells=[8000, 18000, 4500], solution=[5.972, 6.063, 5.863])
+
+
+@pytest.fixture
+def example_grid_celik_with_representative_size():
+    return pyGCS.GCI(dimension=2, grid_size=[0.75, 1.125, 1.5], cells=[18000, 8000, 4500],
+                     solution=[6.063, 5.972, 5.863])
+
+
+def test_gci_calculation_based_on_volume(example_grid_celik):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter['volume'], grid_parameter['grids'], grid_parameter['solution'])
+    sut = example_grid_celik
 
     # act
-    gci = sut.get_gci()
+    gci = sut.get('gci')
 
     # assert
     assert len(gci) == 2
@@ -124,61 +71,87 @@ def test_gci_calculation_based_on_volume(grid_parameter):
     assert 0.0411 < gci[1] < 0.0412
 
 
-def test_asymptotic_gci_calculation(grid_parameter):
+def test_gci_calculation_based_on_single_volume(example_grid_celik_single_volume):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter['volume'], grid_parameter['grids'], grid_parameter['solution'])
+    sut = example_grid_celik_single_volume
 
     # act
-    asymptotic_gci = sut.get_asymptotic_gci()
+    gci = sut.get('gci')
+
+    # assert
+    assert len(gci) == 2
+    assert 0.0217 < gci[0] < 0.0218
+    assert 0.0411 < gci[1] < 0.0412
+
+
+def test_gci_calculation_based_on_representative_size(example_grid_celik_with_representative_size):
+    # arrange
+    sut = example_grid_celik_with_representative_size
+
+    # act
+    gci = sut.get('gci')
+
+    # assert
+    assert len(gci) == 2
+    assert 0.0217 < gci[0] < 0.0218
+    assert 0.0411 < gci[1] < 0.0412
+
+
+def test_asymptotic_gci_calculation(example_grid_celik):
+    # arrange
+    sut = example_grid_celik
+
+    # act
+    asymptotic_gci = sut.get('asymptotic_gci')
 
     # assert
     assert len(asymptotic_gci) == 1
     assert 1.015 < asymptotic_gci[0] < 1.016
 
 
-def test_extrapolated_value(grid_parameter):
+def test_extrapolated_value(example_grid_celik):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter['volume'], grid_parameter['grids'], grid_parameter['solution'])
+    sut = example_grid_celik
 
     # act
-    extrapolated_value = sut.get_extrapolated_value()
+    extrapolated_value = sut.get('extrapolated_value')
 
     # assert
     assert 6.168 < extrapolated_value < 6.169
 
 
-def test_desired_gci_calculation(grid_parameter):
+def test_desired_gci_calculation(example_grid_celik):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter['volume'], grid_parameter['grids'], grid_parameter['solution'])
+    sut = example_grid_celik
+    cells = sut.get('cells')
 
     # act
     cells_for_smaller_gci = sut.get_number_of_cells_for_specified_gci_of(0.01)
     cells_for_larger_gci = sut.get_number_of_cells_for_specified_gci_of(0.03)
 
     # assert
-    assert cells_for_smaller_gci > grid_parameter['grids'][0]
-    assert cells_for_larger_gci < grid_parameter['grids'][0]
+    assert cells_for_smaller_gci > cells[0]
+    assert cells_for_larger_gci < cells[0]
 
 
-def test_order_calculation(grid_parameter):
+def test_order_calculation(example_grid_celik):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter['volume'], grid_parameter['grids'], grid_parameter['solution'])
+    sut = example_grid_celik
 
     # act
-    order = sut.get_order()
+    order = sut.get('apparent_order')
 
     # assert
     assert len(order) == 1
     assert 1.53 < order[0] < 1.54
 
 
-def test_gci_calculation_in_reverse_order(grid_parameter_reverse_order):
+def test_gci_calculation_in_reverse_order(example_grid_celik_reverse_order):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter_reverse_order['volume'], grid_parameter_reverse_order['grids'],
-                    grid_parameter_reverse_order['solution'])
+    sut = example_grid_celik_reverse_order
 
     # act
-    gci = sut.get_gci()
+    gci = sut.get('gci')
 
     # assert
     assert len(gci) == 2
@@ -186,13 +159,12 @@ def test_gci_calculation_in_reverse_order(grid_parameter_reverse_order):
     assert 0.0411 < gci[1] < 0.0412
 
 
-def test_gci_calculation_in_random_order(grid_parameter_random_order):
+def test_gci_calculation_in_random_order(example_grid_celik_random_order):
     # arrange
-    sut = pyGCS.GCI(2, grid_parameter_random_order['volume'], grid_parameter_random_order['grids'],
-                    grid_parameter_random_order['solution'])
+    sut = example_grid_celik_random_order
 
     # act
-    gci = sut.get_gci()
+    gci = sut.get('gci')
 
     # assert
     assert len(gci) == 2
